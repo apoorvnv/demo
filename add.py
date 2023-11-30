d9732a2a37ca6778a2c3106cd470d78536cf5870
Apoorv <apoorvnv@gmail.com>
2023-11-30 13:15:27 +0530

    docstring error fixed in addtion file

--- add.py
+++ add.py
@@ -1,7 +1,13 @@
 # Addition program
 
+""" Take i/p first num """
 a = int(input("Enter first num: "))
+
+"""Take i/p 2n second num"""
 b = int(input("Enter second num: "))
+
+"""sum of 2 num"""
 num = a+b
 
+"""This print statement displays the addition"""
 print("Addition of 2 num is ", num)
