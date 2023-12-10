0e7b10f81e7cfce9fc85ca4ca55e57108f294f8a
shrav20 <91880988+shrav20@users.noreply.github.com>
2023-12-10 19:09:58 +0530

    Create class_name_checker.py

---
+++ class_name_checker.py
@@ -0,0 +1,22 @@
+# class_name_checker.py
+from pylint.checkers import BaseChecker
+
+class ClassNameChecker(BaseChecker):
+    name = 'class-name-checker'
+    priority = -1
+    msgs = {
+        'C9999': (
+            'Class name should start with a verb',
+            'class-name-should-start-with-verb',
+            'Class names should start with a verb.'
+        ),
+    }
+
+    def visit_classdef(self, node):
+        class_name = node.name.lower()
+        verbs = ['create', 'initialize', 'generate', 'compute']  # Add more verbs as needed
+        if not any(class_name.startswith(verb) for verb in verbs):
+            self.add_message('class-name-should-start-with-verb', node=node)
+
+def register(linter):
+    linter.register_checker(ClassNameChecker(linter))
